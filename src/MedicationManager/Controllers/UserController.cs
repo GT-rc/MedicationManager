908b57339b8716a1988ec719e7fad7d7e943f435
Glynnis Thomas <glynnisthomas@gmail.com>
2017-05-21 17:30:07 -0500

    Created UserController and AddUserViewModel, populated; Created MedicationController - empty

---
+++ src/MedicationManager/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MedicationManager.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MedicationManager.Controllers
+{
+    public class UserController : Controller
+    {
+        public static string username;
+
+        // GET: /<controller>/
+        [Route("/Login")]
+        public IActionResult Index()
+        {
+            ViewBag.username = username;
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            AddUserViewModel addUserViewModel = new AddUserViewModel();
+            return View(addUserViewModel);
+        }
+
+        [HttpPost]
+        [Route("/Login/Add")]
+        public IActionResult Add(AddUserViewModel addUserViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                username = addUserViewModel.Username;
+
+                //TODO: Create User
+
+                return Redirect("/MedHome");
+            }
+
+            return View(addUserViewModel);
+        }
+    }
+}
---
+++ src/MedicationManager/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicationManager.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Display(Name = "Username: ")]
+        [Required]
+        public string Username { get; set; }
+
+        [Display(Name = "Full Name")]
+        [Required]
+        public string FullName { get; set; }
+
+        [Display(Name = "Email Address: ")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Password: ")]
+        [DataType(DataType.Password)]
+        [StringLength(25, ErrorMessage = "Must be between 6 and 25 characters", MinimumLength = 6)]
+        [Required]
+        public string Password { get; set; }
+
+        [Display(Name = "Verify Password: ")]
+        [DataType(DataType.Password)]
+        [StringLength(25, ErrorMessage = "Must be between 6 and 25 characters", MinimumLength = 6)]
+        [Compare("Password", ErrorMessage = "The passwords you entered do not match!")]
+        [Required]
+        public string VerifyPassword { get; set; }
+
+        public string UserId { get; set; }
+
+
+    }
+}

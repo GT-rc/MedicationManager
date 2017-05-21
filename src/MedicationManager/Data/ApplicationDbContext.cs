04fc42dbbbf81bb751fe6fa50f6de09f5c4b5077
Glynnis Thomas <glynnisthomas@gmail.com>
2017-05-21 18:34:04 -0500

    Created ApplicationDbContext

---
+++ src/MedicationManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using MedicationManager.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicationManager.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Medication> Medication { get; set; }
+        public DbSet<MedSets> MedSets { get; set; }
+        public DbSet<Set> Set { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            
+            builder.Entity<MedSets>().HasKey(c => new { c.UserId, c.SetId });
+        }
+    }
+}

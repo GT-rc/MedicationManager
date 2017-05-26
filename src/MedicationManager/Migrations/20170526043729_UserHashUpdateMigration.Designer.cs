2c578e51b257ab396e0b0c056ead27573744474d
Glynnis Thomas <glynnisthomas@gmail.com>
2017-05-25 23:38:13 -0500

    Did Db Migration and Update

--- src/MedicationManager/Migrations/20170526043729_UserHashUpdateMigration.Designer.cs
+++ src/MedicationManager/Migrations/20170526043729_UserHashUpdateMigration.Designer.cs
@@ -8,9 +8,10 @@ using MedicationManager.Data;
 namespace MedicationManager.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170526043729_UserHashUpdateMigration")]
+    partial class UserHashUpdateMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
